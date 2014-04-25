24b3013c32cb01e49ef3b02fc03fa0982a9e66be
radnotiz <radnoti.zoltan@gmail.com>
2014-04-25 15:34:49 +0200

    added gradle

---
+++ src/main/java/com/github/radnotiz/justeat_test/step_definitions/FrontPage.java
@@ -0,0 +1,34 @@
+package com.github.radnotiz.justeat_test.step_definitions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zradnoti
+ * Date: 4/25/14
+ * Time: 2:45 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FrontPage {
+
+    @Given("^I want food in \"([^\"]*)\"$")
+    public void I_want_food_in(String arg1) throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+
+    @When("^I search for restaurants$")
+    public void I_search_for_restaurants() throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+
+    @Then("^I should see some restaurants in \"([^\"]*)\"$")
+    public void I_should_see_some_restaurants_in(String arg1) throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+}
