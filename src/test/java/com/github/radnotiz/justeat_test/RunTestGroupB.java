b5daeefb73bf10935267ea1b4c3547681cdd3324
radnotiz <radnoti.zoltan@gmail.com>
2014-04-27 10:35:08 +0200

    made tests running in parallel

---
+++ src/test/java/com/github/radnotiz/justeat_test/RunTestGroupB.java
@@ -0,0 +1,11 @@
+package com.github.radnotiz.justeat_test;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/com/github/radnotiz/justeat_test/features",
+        tags = {"@groupB"})
+public class RunTestGroupB {
+}
